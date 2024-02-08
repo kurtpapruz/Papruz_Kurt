4d2b0e470e4895bb263f0f5251d641b4c579016e
papruz.289857 <papruz.289857@LAB-A-PC26>
2024-02-08 08:05:28 +0800

    Add project files.

---
+++ Papruz_Kurt/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Papruz_Kurt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter the total number of your enrolled courses: ");
+            int course;
+            course = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the price of your favorite  books: ");
+            double price;
+            price = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Total enrolled course: " + course);
+            Console.WriteLine("Price of my favorite book: " + price);
+            
+        }
+    }
+}
